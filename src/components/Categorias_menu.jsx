ec16eb03dac3cc4e77b87b459a5dc2a58f39adbf
antoniorr <antoniorr@correo.ugr.es>
2025-03-04 19:33:23 +0100

    Mejorando estilo de categorias #12

--- src/components/Categorias_menu.jsx
+++ src/components/Categorias_menu.jsx
@@ -82,8 +82,8 @@ export default function CategoryView() {
           </ul>
         )}
       </aside>
-      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-        {loadingProductos ? (
+      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mx-auto max-w-screen-xl">
+      {loadingProductos ? (
           <p>Cargando productos...</p>
         ) : productos.length > 0 ? (
           productos.map((producto) => (
